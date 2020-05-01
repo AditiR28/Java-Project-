a85cb5eff60810f62663ee3110f9727df483c082
AditiR28 <rajput.28aditi@gmail.com>
2020-05-01 15:14:29 +0530

    myfirstcode.java

---
+++ MyFirstCode.java
@@ -0,0 +1,19 @@
+public class MyFirstCode{
+  public static void main(String[] args) {
+	  String name="Aditi Bloodhound DMS" ;
+	  int myNum=20;
+	  myNum=15;
+	  float myclass= 3.383339939f;
+	  char my = 'A';
+	  boolean nice= true;
+	  
+	  System.out.println(myNum);
+	  System.out.println(myclass);
+	  System.out.println(my);
+	  System.out.println(nice);
+	  
+	  System.out.println("Aditi");
+	 System.out.println("Bloodhound ,DMS");
+    System.out.println("FirstCodeJava"); 
+  }
+}
